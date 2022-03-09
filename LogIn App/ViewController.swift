d985dd8f4888cce1e23e5846e25708e65a3f629b
Kirill <frolkv@gmail.com>
2022-03-09 14:06:20 +0300

    user/password buttons

--- LogIn App/ViewController.swift
+++ LogIn App/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showUser() {
+        showAlert(title: "Ooops", message: "Your User name is Alex")
+    }
+    @IBAction func showPassword() {
+        showAlert(title: "Ooops", message: "Your User password is 123")
+    }
+    
 }
 
 // MARK: - Private Methods
